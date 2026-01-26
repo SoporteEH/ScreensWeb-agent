4e0d7be56bb4c43c3bcee608313932dc3a0d5371
soporteeh <soporte.ehdev@gmail.com>
2026-01-26 14:49:17 +0100

    Version V1.0.14

--- local-agent/preload.js
+++ local-agent/preload.js
@@ -1,7 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
-    // Ventana de identificación
+    // Identificación
     onDeviceId: (callback) => ipcRenderer.on('device-id', (event, ...args) => callback(...args)),
 
     // Panel de control
@@ -9,15 +9,9 @@ contextBridge.exposeInMainWorld('electron', {
     onUpdateStatus: (callback) => ipcRenderer.on('update-status', (event, ...args) => callback(...args)),
 
     sendAction: (action, data) => ipcRenderer.send('agent-action', { action, data }),
-
-    // Obtener versión de la app
     getAppVersion: () => ipcRenderer.invoke('get-app-version'),
-
-    // Controles de ventana
     minimizeWindow: () => ipcRenderer.send('window-control', 'minimize'),
     closeWindow: () => ipcRenderer.send('window-control', 'close'),
-
-    // Comunicación genérica
     send: (channel, data) => ipcRenderer.send(channel, data),
     on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args))
 });
